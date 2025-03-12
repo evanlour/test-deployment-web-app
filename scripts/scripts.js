fa03b8f1d87ff60977a48e3b90b18d48f7f8f940
evanlour <138373709+evanlour@users.noreply.github.com>
2025-03-12 23:20:37 +0200

    test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -110,7 +110,7 @@ submitButton.addEventListener('click', () => {
 
   // Convert to base64 PNG (now guaranteed to be RGB)
   const dataURL = tempCanvas.toDataURL('image/png');
-  fetch('https://letterandtextrecognition.loca.lt/predict', {
+  fetch('https://crazy-comics-brush.loca.lt/predict', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',  // Send data as JSON
