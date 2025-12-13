ea311e2105ce273e0d30caf075bf51d0f550cd4c
Evangelos Lourmpakis <evanlourb@gmail.com>
2025-12-13 03:12:36 +0200

    fixed subrouter

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/test-deployment-web-app">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
